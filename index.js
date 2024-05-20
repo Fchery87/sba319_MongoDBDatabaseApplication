8b68be797dee6a71c085ba9b838794a976f42366
Frantz Chery <frantzchery@hotmail.com>
2024-05-20 15:17:18 -0400

    Initial SBA

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import usersRouter from './routes/users.js';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+//! ====== MIDDLEWARE ======
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Request from url: ' + req.url);
+  next();
+});
+
+//! ====== ROUTES ======
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the SBA 319');
+});
+
+app.use('/users', usersRouter);
+
+//! ====== GLOBAL HANDLING ======
+
+app.use((err, _req, res, next) => {
+  res.status(500).send('Server Error!');
+});
+
+//! ====== LISTENING PORT ======
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ routes/users.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import db from '../db/conn.js';
+import { ObjectId } from 'mongodb';
+
+const router = express.Router();
+
+export default router;
